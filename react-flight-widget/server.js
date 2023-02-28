9569e61028acedbf431fbaeb45c60c7c5c4dd65f
handomekal <handomekal@gmail.com>
2023-02-28 16:27:09 -0500

    Add get request and catch error

--- react-flight-widget/server.js
+++ react-flight-widget/server.js
@@ -6,4 +6,21 @@ require('dotenv').config()
 
 const app = express()
 
+app.get('/flights', (req, res) => {
+    const options = {
+        url: 'https://8539fbcf-bac2-45fa-bad8-307125861780-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/flights/collections/departures',
+        headers: {
+            accept: 'application/json',
+            'X-Cassandra-Token': 'AstraCS:oiXeQiNxtOzzDZrvxqkAjRSa:0411510411b5c2a0fae9ad3ddc3c772ca97e5e16ffc58177d9ba9cecfcb05381' 
+        }
+    }
+
+    axios.request(options).then(response => {
+        console.log(response.data)
+        res.json(response.data)
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
 app.listen(PORT, () => console.log('running on port' + PORT))
