70dc9b62f90be05366ddb51860c03315486eef9b
handomekal <handomekal@gmail.com>
2023-02-28 15:23:39 -0500

    Add express

--- react-flight-widget/server.js
+++ react-flight-widget/server.js
@@ -3,3 +3,7 @@ const axios = require('axios').default
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+
+const app = express()
+
+app.listen(PORT, () => console.log('running on port' + PORT))
